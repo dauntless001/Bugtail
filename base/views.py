8c90895cd6244c69dbf85d18f65d23fc04576afd
dauntless <suntopmultimedia@gmail.com>
2021-12-17 09:57:59 +0100

    project list truncation on dashboard

--- base/views.py
+++ base/views.py
@@ -6,7 +6,9 @@ from bugtail.helpers.project_helper import get_user_projects
 
 class DashboardView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
+        projects = get_user_projects(self.request.user)[:8]
         context = {
-            'projects':get_user_projects(self.request.user)
+            'projects':projects,
+            'view_all': True if projects.count() > 8 else False
         }
         return render(request, 'index.html', context)
