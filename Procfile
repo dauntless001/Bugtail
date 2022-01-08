03a7040b11f149e6a2921d61d2dbc6ba0187630a
dauntless <suntopmultimedia@gmail.com>
2022-01-08 09:58:59 +0100

    production settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bugtail.wsgi --preload --log-file -
--- bugtail/settings/production.py
+++ bugtail/settings/production.py
@@ -1,2 +1,26 @@
 from bugtail.settings.base import *
 from bugtail.helpers.email_helper import *
+from bugtail.settings.packages.aws import *
+import os, dj_database_url
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os,os.getenv('DEBUG', 'False') == 'True'
+
+INSTALLED_APPS.append("storages")
+
+MIDDLEWARE.insert(2, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+DATABASES['default'] = dj_database_url.parse(url=os.getenv('DATABASE_URL'), conn_max_age=600)
+
+STATIC_URL = '/static/'
+STATIC_ROOT = Path.cwd().joinpath(BASE_DIR).joinpath('assets')
+STATICFILES_DIRS = [
+    Path.cwd().joinpath(BASE_DIR).joinpath('bugtail/assets'),
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path.cwd().joinpath(BASE_DIR).joinpath('media')
